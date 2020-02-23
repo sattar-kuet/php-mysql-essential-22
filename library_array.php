d4117d20cee82337201c26b81ac3d009e267d0e1
Abdus Sattar Bhuiyan <sattar.kuet@gmail.com>
2020-02-23 14:58:12 +0600

    first commit

---
+++ library_array.php
@@ -0,0 +1,32 @@
+<?php
+$data = array(10,2,3,5,6,7,90,-10);
+
+// echo count($data);
+//echo max($data);
+//echo min($data);
+//sort($data);
+//rsort($data);
+
+//echo '<pre>';print_r($data); echo '</pre>';
+ // $str = implode("*", $data);
+  //echo $str;
+//10*2*3*5*6*7*90*-10
+//   $arr = explode("*", $str);
+ //echo '<pre>';print_r($arr); echo '</pre>';
+// $condition = in_array(100,$data);
+
+// if($condition){
+// 	echo 'found';
+// }else{
+// 	echo 'Not found';
+// }
+
+$status = false; // $a = 10; $a>10
+if($status){
+	echo 'inside if';
+}
+else{
+	echo 'inside else';
+}
+
+?>
